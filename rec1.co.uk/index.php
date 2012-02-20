d158c7653b18e9640c9669c1b829ad8dc8857759
Brian Crocker <briancrocker@southdevon.ac.uk>
2012-02-20 13:41:10 +0000

    altered stylesheet to change offline text colour

---
+++ rec1.co.uk/index.php
@@ -0,0 +1,8 @@
+<?php require_once("../assets/includes/head.php"); ?>
+<?php 
+  if ($result['offline'] == 1) {
+    require_once("../assets/includes/offline.php");
+  } else {
+    require_once("../assets/includes/main_page.php");
+  }
+?>
