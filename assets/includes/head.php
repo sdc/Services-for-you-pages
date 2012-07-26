c50d733ccbddf5dc0105d11a912c55f045e18d37
Brian Crocker <briancrocker@southdevon.ac.uk>
2012-07-26 12:00:44 +0100

    added new fucntionality for social media buttons and the ability to increase div size if no footer is required

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -22,19 +22,7 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript" charset="utf-8"></script>
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=<?php echo $result['google_font']; ?>" /> 
 <script src="<?php echo $file_link; ?>/js/slideshow.js" type="text/javascript"></script>
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?php echo $result['google_analytics']; ?>']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 
 <!-- CSS values set from Admin system -->
 <?php require_once("stylesheet.php"); ?>  
--- assets/includes/main_page.php
+++ assets/includes/main_page.php
@@ -55,48 +55,122 @@
     </div>
     <div class="right">
       <h1 style="font-family:<?php echo $result['google_font']; ?>; color:#<?php echo $result['h1_color']; ?>;font-size:<?php echo $result['h1_font_size']; ?>em; text-shadow:<?php echo $result['main_h1_drop_shadow']; ?>; margin: <?php echo $result['content_header_margin']; ?>;"><?php echo $result['latest_news_header']; ?></h1>
-      <div style="">
+      <div id="twitter-widget">
         <script type="text/javascript" src="http://widgets.twimg.com/j/2/widget.js"></script>
-          <script type="text/javascript">
-            new TWTR.Widget({
-              version: 2,
-              type: 'profile',
-              rpp: 4,
-              interval: 6000,
-              width: 250,
-              height: 180,
-              theme: {
-                shell: {
-                  background: '#<?php echo $result['theme_background_color']; ?>',
-                  color: '#<?php echo $result['theme_font_color']; ?>'
-                },
-                tweets: {
-                  background: '#<?php echo $result['tweet_background_color']; ?>',
-                  color: '#<?php echo $result['tweet_font_color']; ?>',
-                  links: '#<?php echo $result['twiter_link_color']; ?>'
-                }
+        <script type="text/javascript">
+          new TWTR.Widget({
+            version: 2,
+            type: 'profile',
+            rpp: 4,
+            interval: 6000,
+            width: 250,
+            height: 180,
+            theme: {
+              shell: {
+                background: '#<?php echo $result['theme_background_color']; ?>',
+                color: '#<?php echo $result['theme_font_color']; ?>'
               },
-              features: {
-                scrollbar: true,
-                loop: false,
-                live: false,
-                hashtags: true,
-                timestamp: true,
-                avatars: false,
-                behavior: 'list'
+              tweets: {
+                background: '#<?php echo $result['tweet_background_color']; ?>',
+                color: '#<?php echo $result['tweet_font_color']; ?>',
+                links: '#<?php echo $result['twiter_link_color']; ?>'
               }
-            }).render().setUser('<?php echo $result['twitter_user']; ?>').start();
+            },
+            features: {
+              scrollbar: true,
+              loop: false,
+              live: false,
+              hashtags: true,
+              timestamp: true,
+              avatars: false,
+              behavior: 'list'
+            }
+          }).render().setUser('<?php echo $result['twitter_user']; ?>').start();
         </script>
+      </div>
+      <div id="social_media_buttons">
+          <?php 
+            if (empty($result['sm_button_1'])) {
+              echo '';
+            } else {
+              echo '
+                <div class="sm_button" id="1">
+                  <a href="' . $result['sm_button_1_link'] . '" target="_blank">
+                    <img src="images/' . $result['sm_button_1'] . '" /> 
+                  </a>
+                </div>';
+            }
+          ?>
+          <?php 
+            if (empty($result['sm_button_2'])) {
+              echo '';
+            } else {
+              echo '
+                <div class="sm_button" id="2">
+                  <a href="' . $result['sm_button_2_link'] . '" target="_blank">
+                    <img src="images/' . $result['sm_button_2'] . '" /> 
+                  </a>
+                </div>';
+            }
+          ?>
+          <?php 
+            if (empty($result['sm_button_3'])) {
+              echo '';
+            } else {
+              echo '
+                <div class="sm_button" id="3">
+                  <a href="' . $result['sm_button_3_link'] . '" target="_blank">
+                    <img src="images/' . $result['sm_button_3'] . '" /> 
+                  </a>
+                </div>';
+            }
+          ?>
+          <?php 
+            if (empty($result['sm_button_4'])) {
+              echo '';
+            } else {
+              echo '
+                <div class="sm_button" id="4">
+                  <a href="' . $result['sm_button_4_link'] . '" target="_blank">
+                    <img src="images/' . $result['sm_button_4'] . '" /> 
+                  </a>
+                </div>';
+            }
+          ?>
       </div>
     </div>
   </div>
-  <div id="footer">
-    <div id="footer_content">
-      <?php echo $result['footer_image']; ?>
-      <?php echo $result['footer_text']; ?>
-    </div>
-  </div>
+  <?php if ($result['footer_required'] == 1) { ?>
+    <div id="footer">
+      <div id="footer_content">
+        <?php echo $result['footer_image']; ?>
+        <?php echo $result['footer_text']; ?>
+      </div>
+    </div>      
+  <?php } else { ?>
+    <style type="text/css">
+    <!-- 
+      #main_content {
+           height: 500px;
+           margin-bottom: 30px;
+      
+      -->
+    </style>
+  <?php } ?>  
 </div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo $result['google_analytics']; ?>']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 
 </html> 
